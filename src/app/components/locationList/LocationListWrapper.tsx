47c09f5484c1d3edaad517333910935b89c770e4
hwan12321 <kdh643737@gmail.com>
2025-10-10 16:03:04 +0900

    fix: locationDetail 이미지업로드 후 전체일정에 바로 반영안되는 부분 수정

--- src/app/components/locationList/LocationListWrapper.tsx
+++ src/app/components/locationList/LocationListWrapper.tsx
@@ -15,7 +15,7 @@ const LocationListWrapper = ({ selectedDay, totalLocationList, setLocation} : TP
         return <LocationList day={parseInt(selectedDay, 10) - 1} isTotal={false} locationList={totalLocationList[parseInt(selectedDay, 10) - 1]} setLocation={setLocation} orderColor={getOrderColor(parseInt(selectedDay, 10) - 1)} totalLocationList={totalLocationList}/>
     } else {
         return <div className={style.locationlist_list}>
-            {totalLocationList.map((locationList,index) => {if(locationList.length > 0) return <div className={style.locationlist_item}><span className={style.day}>{index+1}일차</span><LocationList day={0} isTotal={true} totalLocationList={totalLocationList} locationList={locationList} setLocation={setLocation} orderColor={getOrderColor(index)}/></div>})}
+            {totalLocationList.map((locationList,index) => {if(locationList.length > 0) return <div className={style.locationlist_item}><span className={style.day}>{index+1}일차</span><LocationList day={index} isTotal={true} totalLocationList={totalLocationList} locationList={locationList} setLocation={setLocation} orderColor={getOrderColor(index)}/></div>})}
             {totalLocationList.flat().length === 0 && <NoResult desc=""/>}
         </div>
     }
